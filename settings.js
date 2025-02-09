0408b490b95b94d2dad7df66af686fcad09166fb
ahmadgondal925 <ahmadtypist786@gmail.com>
2025-02-09 22:00:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Ahmad gondal', // Your name
-  ownerNumber: '+923480634848', //Your number
+  ownerNumber: '+923480634838', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
